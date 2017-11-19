d42c8122a0dba2f8d86e7bcec68bf9c7541d99e7
sergiokov20 <sergiokovf1champs@gmail.com>
2017-11-19 16:37:39 +0100

    Integrat EditarPerfil amb MiPerfil (profile.ejs)

---
+++ public/js/profile.js
@@ -0,0 +1,15 @@
+function switchtoedit() {
+  document.getElementById('perfil').style.display = 'none';
+  document.getElementById('editar').style.display = 'block';
+}
+function switchtoprofilenoedit() {
+  document.getElementById('perfil').style.display = 'block';
+  document.getElementById('editar').style.display = 'none';
+}
+function switchtoprofileedit() {
+  document.getElementById('perfil').style.display = 'block';
+  document.getElementById('editar').style.display = 'none';
+
+  /*TO DO: codi per actualitzar les dades a la Base de Dades */
+
+}
