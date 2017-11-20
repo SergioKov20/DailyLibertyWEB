8e39652caf5e95bddaa652b39082d2dea5fdb113
Edgar <edgarlorenzo17@gmail.com>
2017-11-20 15:53:34 +0100

    Creo directori proves al views per tenir htmls que estic fent allà, i modifico els noms de lss css i la referencia en els ejs. Faltaria canvia el de styleArticle un cop no es faci servir el article.css de les proves.

--- prova/views/editarperfil/setdefaulttext.js
+++
@@ -1,7 +0,0 @@
-//Función que consigue los valores actuales del perfil para ponerlos en los textarea
-//Los valores actuales son de prueba, hay que hacerlo para que los reciba de la BD Mongo
-
-document.getElementById('ta1').value = 'Sergio Rodriguez';
-document.getElementById('ta2').value = 'sergio@maildeprova.com';
-document.getElementById('ta3').value = '20/02/1981';
-document.getElementById('ta4').value = 'I love writting articles in DailyLiberty!';
