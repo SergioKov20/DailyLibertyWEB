4a756f8f17464253edf7d13c5c6a2c31c9419e4d
thebasto17 <info.marc32@gmail.com>
2017-11-23 13:55:49 +0100

    Add files via upload
    js de article

---
+++ public/js/article.js
@@ -0,0 +1,11 @@
+function myFunction() {
+                window.open('./vista_previa_article.ejs','_blank')              
+}
+
+
+function cancel(){
+    
+                alert("Are you sure to discard all the content?")
+                window.open('./index.ejs',"_self")
+    
+}
