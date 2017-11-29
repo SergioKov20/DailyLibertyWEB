8f67f06049dc95c64e69b2139083298b3e6026d9
Edgar <edgarlorenzo17@gmail.com>
2017-11-29 16:47:17 +0100

    s'implementa el replace dels insults amb array i té en compte també els insults si es repeteixen( falta augmentar la llista) Entenc que s'haurà d'integrar en totes les pàgines on hi hagi articles(index, tendencies..etc)

---
+++ public/js/correctorInsults.js
@@ -0,0 +1,9 @@
+var arrayInsults = ["putas","mal parit", "soplagaites", "mala fembra", "imbècil"];
+var str1 = "*****";
+var arrayLength = arrayInsults.length;
+for (var i = 0; i < arrayLength; i++) {
+    
+    var insultRepetit = new RegExp(arrayInsults[i], "gi");
+    var newhtml = $('#global').html().replace(insultRepetit, '<span class="marked">' + str1 + '</span>');
+    $('#global').html(newhtml);
+}
