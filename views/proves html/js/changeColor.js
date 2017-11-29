072b038bf111614e6d84c069b784a2bdbca611dc
Edgar <edgarlorenzo17@gmail.com>
2017-11-29 01:25:43 +0100

    Implementació like/dislike segons com li arriba el estat del like. Es modifica tant el js com el ejs (en el ejs estan les 3 possibilitats comentades es descomentarles i borrar la actual. Ho he deixat sense posar variable likes dislikes per si vols fer prova visual

--- views/proves html/js/changeColor.js
+++ views/proves html/js/changeColor.js
@@ -1,13 +1,32 @@
+//Like & Undislike funciona
+//Dislike & Unlike funciona
+
 $('#muteGreen').click(function() {
-    //$('#nogreen').toggleClass('likenogreen');
+    $('#nogreen').toggleClass('likenogreen');
+    $('#nogreen').toggleClass('likegreen');
+    $('#nored').removeClass('dislikered');
+    $('#nored').addClass('dislikenored');
+});
+
+$('#muteRed').click(function() {
+    $('#nored').toggleClass('dislikenored');
+    $('#nored').toggleClass('dislikered');
+    $('#nogreen').removeClass('likegreen');
+    $('#nogreen').addClass('likenogreen');
+});
+
+
+//Unlike i undislike funciona
+
+$('#muteNoGreen').click(function() {
     $('#nogreen').toggleClass('likegreen');
     $('#nored').removeClass('dislikered');
 });
 
-$('#muteRed').click(function() {
-    //$('#nored').toggleClass('dislikenored');
+$('#muteNoRed').click(function() {
     $('#nored').toggleClass('dislikered');
     $('#nogreen').removeClass('likegreen');
 });
+
 
 
